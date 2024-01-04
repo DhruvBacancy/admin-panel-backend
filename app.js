c3f2b6fac568e676576413a5346bed781b03ddb3
Dhruv Patel <dhruv.patel@bacancy.com>
2024-01-04 14:07:39 +0530

    body - parser setup

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const http = require("http")
+const cors = require("cors")
+const bodyParser = require("body-parser")
 
 require("dotenv").config()
 require("./src/config/sequelize-dbconnect")
 
 const app = express()
 const server = http.createServer(app)
+
+app.use(cors())
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.use(bodyParser.json())
 
 module.exports = server
