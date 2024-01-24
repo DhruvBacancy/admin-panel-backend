164521823d067439da79822e4c098f7bbf572b8d
Dhruv Patel <dhruv.patel@bacancy.com>
2024-01-24 11:23:59 +0530

    app moducle folder structure change

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { AuthModule } from '../auth/auth.module'
+import { UserOperationsModule } from '../user-operations/user-operations.module'
+import { Module } from '@nestjs/common'
+import * as dotenv from 'dotenv'
+import { DatabaseModule } from '../database/database.module'
+import { AppService } from './app.service';
+
+dotenv.config()
+
+@Module({
+  imports: [DatabaseModule, AuthModule, UserOperationsModule],
+  providers: [AppService],
+})
+export class AppModule {}
